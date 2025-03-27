2715c9b9f5705f631336b936d6646fa2e3b8b326
PHSTD <c.oadib.autoan123@gmail.com>
2025-03-28 00:32:17 +0900

    update

---
+++ Day250327_1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel.Design;
+
+namespace Day250327_1;
+
+class Program
+{
+    // <인덱서 정의>
+    // this[]를 속성으로 정의하여 클래스의 인스턴스에 인덱스 방식으로 접근 허용
+    public class Indexer
+    {
+        private int[] array = new int[10];
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+    }
+
+    // <인덱서 자료형>
+    // 인덱서는 다른 자료형 사용도 가능
+    // 열거형을 통해 인덱서를 사용하는 경우도 빈번
+    public enum Parts { Head, Body, Feet, Hand, SIZE }
+    public class Equipment
+    {
+        string[] parts = new string[(int)Parts.SIZE];
+
+        public string this[Parts type]
+        {
+            get
+            {
+                return parts[(int)type];
+            }
+            set
+            {
+                parts[(int)type] = value;
+            }
+        }
+    }
+
+  
+        
+
+        public static void Main()
+        {
+            Equipment equipment = new Equipment();
+
+            equipment[Parts.Hand] = "투구";
+            equipment[Parts.Body] = "갑옷";
+
+            string curHead = equipment[Parts.Head];
+            
+        }
+}
+
---
+++ Day250327_team_alone/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace Day250327_team_alone;
+
+class Program
+{
+    public enum Parts {Head, Body, Arm, Leg, Hand, Foot, Size}; 
+    
+    class Player
+    {
+        private int hp = 10;
+        private Armor armor;
+
+        private event Action OnHit;
+        
+        public void Equip(Armor armor)
+        {
+            this.armor = armor;
+            OnHit += armor.Hit;
+        }
+
+        public void Hit()
+        {
+            Console.WriteLine("플레이어가 공격 받았습니다.");
+            hp -= 1;
+            int totalHp = hp <= 0 ? 0 : hp;
+            hp = totalHp;
+            Console.WriteLine($"플레이어의 체력은{hp} 입니다.");
+            OnHit?.Invoke();
+        }
+
+        public void Die()
+        {
+            Console.WriteLine("죽었습니다.");
+        }
+        
+        public void UnEquip()
+        {
+            Console.WriteLine("갑옷이 없습니다.");
+        }
+    }
+
+    class Armor
+    {
+        private int durability = 3;
+        
+        private Action OnHit;
+
+        public void Brake()
+        {
+            Console.WriteLine("방어구가 깨졌습니다");
+            OnHit?.Invoke();
+        }
+
+        public void Hit()
+        {
+            if (durability <= 0)
+            {
+                Brake();
+            }
+            else
+            {
+                Console.WriteLine($"내구성이 {durability}에서 {--durability} 으로 깍였습니다.");
+            }
+        }
+    }
+    
+    static void Main(string[] args)
+    {
+        Player player = new Player();
+        Armor armor = new Armor();
+        
+        player.Equip(armor);
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+        player.Hit();
+    }
+}
