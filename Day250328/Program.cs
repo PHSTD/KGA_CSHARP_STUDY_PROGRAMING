353d0e9245f2bb7af87448c6fc4f1f5824f30d9b
PHSTD <c.oadib.autoan123@gmail.com>
2025-03-28 20:17:55 +0900

    update

---
+++ Day250328/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day250328;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 과제
+        Training training = new Training();
+        
+        int n = 8;
+        int k = 5;
+        int result = training.Yosepus(n, k);
+        Console.WriteLine("요세푸스({0}, {1}) 의 결과는 : {2}", n, k, result);
+
+        string str = "([])";
+        Console.WriteLine("괄호 검사기의 결과는 : {0}", training.IsCorrectBracket(str));
+    }
+}
